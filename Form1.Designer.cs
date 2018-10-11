3e7be144cb5255e1cabb79a50dc1e0b9935d912a
Daniele Esposti <daniele.esposti@gmail.com>
2018-10-11 16:01:22 +0100

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace WindowsFormsApp3
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.txtBody = new System.Windows.Forms.TextBox();
+			this.SuspendLayout();
+			// 
+			// txtBody
+			// 
+			this.txtBody.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.txtBody.Location = new System.Drawing.Point(0, 0);
+			this.txtBody.Multiline = true;
+			this.txtBody.Name = "txtBody";
+			this.txtBody.Size = new System.Drawing.Size(800, 450);
+			this.txtBody.TabIndex = 0;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(800, 450);
+			this.Controls.Add(this.txtBody);
+			this.Name = "Form1";
+			this.Text = "Windows Credential Manager Viewer";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.TextBox txtBody;
+	}
+}
+
---
+++ Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.InteropServices;
+using Microsoft.Win32.SafeHandles;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp3
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+
+			txtBody.Text = EnumerateCrendentials();
+		}
+
+		[DllImport("Advapi32.dll", EntryPoint = "CredReadW", CharSet = CharSet.Unicode, SetLastError = true)]
+		static extern bool CredRead(string target, CredentialType type, int reservedFlag, out IntPtr credentialPtr);
+
+		[DllImport("advapi32", SetLastError = true, CharSet = CharSet.Unicode)]
+		static extern bool CredEnumerate(string filter, int flag, out int count, out IntPtr pCredentials);
+
+		[DllImport("Advapi32.dll", EntryPoint = "CredFree", SetLastError = true)]
+		static extern bool CredFree([In] IntPtr cred);
+
+		[StructLayout(LayoutKind.Sequential, CharSet = CharSet.Unicode)]
+		private struct CREDENTIAL
+		{
+			public UInt32 Flags;
+			public CredentialType Type;
+			public IntPtr TargetName;
+			public IntPtr Comment;
+			public System.Runtime.InteropServices.ComTypes.FILETIME LastWritten;
+			public UInt32 CredentialBlobSize;
+			public IntPtr CredentialBlob;
+			public UInt32 Persist;
+			public UInt32 AttributeCount;
+			public IntPtr Attributes;
+			public IntPtr TargetAlias;
+			public IntPtr UserName;
+		}
+
+		public enum CredentialType
+		{
+			Generic = 1,
+			DomainPassword,
+			DomainCertificate,
+			DomainVisiblePassword,
+			GenericCertificate,
+			DomainExtended,
+			Maximum,
+			MaximumEx = Maximum + 1000,
+		}
+		
+		public static string EnumerateCrendentials()
+		{
+			string output = "";
+			
+			int count;
+			IntPtr pCredentials;
+			bool ret = CredEnumerate(null, 0, out count, out pCredentials);
+			if (ret)
+			{
+				for (int n = 0; n < count; n++)
+				{
+					IntPtr credentialI = Marshal.ReadIntPtr(pCredentials, n * Marshal.SizeOf(typeof(IntPtr)));
+					CREDENTIAL credential = (CREDENTIAL) Marshal.PtrToStructure(credentialI, typeof(CREDENTIAL));
+
+					string applicationName = Marshal.PtrToStringUni(credential.TargetName);
+					string userName = Marshal.PtrToStringUni(credential.UserName);
+					string secret = null;
+					if (credential.CredentialBlob != IntPtr.Zero)
+					{
+						secret = Marshal.PtrToStringUni(credential.CredentialBlob, (int)credential.CredentialBlobSize / 2);
+					}
+
+					output += string.Format("CredentialType: {0}, ApplicationName: {1}, UserName: {2}, Password: {3}", credential.Type, applicationName, userName, secret) + "\r\n";
+				}
+			}
+			else
+			{
+				int lastError = Marshal.GetLastWin32Error();
+				throw new Win32Exception(lastError);
+			}
+
+			return output;
+		}		
+	}
+}
